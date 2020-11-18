8d5b3c2f45dc75a4b19ea82f34d6de0eb471ca6b
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-11-17 22:12:56 -0600

    Bump up youtube-dl version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=[
         "beets>=1.4.9",
         "confuse>=1.3.0",
-        "youtube-dl>=2020.11.17",
+        "youtube-dl>=2020.11.18",
     ],
 
     package_data={
