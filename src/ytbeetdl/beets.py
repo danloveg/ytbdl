74ec53720e0c83a130c06e03fb2ac9e2043ba458
Daniel Lovegrove <d.lovegrove11@gmail.com>
2021-03-26 12:46:42 -0500

    Load config as BytesIO when beets starts up

--- src/ytbeetdl/beets.py
+++ src/ytbeetdl/beets.py
@@ -1,39 +1,33 @@
 import shutil
+from io import BytesIO
 from pathlib import Path
 from types import SimpleNamespace
+
+import yaml
 
 from beets import config as beetsconfig
 from beets.ui import _setup
 from beets.ui.commands import import_func
+from confuse.yaml_util import load_yaml, Loader
+from confuse.sources import ConfigSource
 
 from ytbeetdl import beetsplug
 from ytbeetdl import config, config_exists, get_main_config_path
 from ytbeetdl.exceptions import ConfigurationError
 
 
-def get_beetsplug_dir() -> Path:
-    return str(Path(beetsplug.__file__).parent.resolve()).replace('\\', '/')
+def set_file_monkeypatch(filename):
+    if isinstance(filename, BytesIO):
+        file_path = None
+        bytes_io = filename
+        yaml_data = yaml.load(bytes_io, Loader=Loader)
+    else:
+        file_path = str(Path(filename).resolve())
+        yaml_data = load_yaml(file_path, loader=Loader)
+    beetsconfig.set(ConfigSource(yaml_data, file_path))
 
-def get_beets_config_path() -> Path:
-    config_path = Path(beetsconfig.user_config_path())
-    if not config_path.exists():
-        config_path.touch()
-    return config_path.resolve()
 
-def overwrite_beets_config(new_contents: str) -> Path:
-    beets_config_path = get_beets_config_path()
-    backup_path = (beets_config_path.parent / (beets_config_path.name + '.bkp')).resolve()
-    shutil.move(beets_config_path, backup_path)
-    with open(beets_config_path, 'w') as write_handle:
-        write_handle.write(new_contents)
-    return backup_path
-
-def restore_backup_beets_config(backup_path: Path):
-    if not backup_path.exists():
-        raise FileNotFoundError(f'Backup beets config file "{backup_path}" does not exist')
-    shutil.move(backup_path, get_beets_config_path())
-
-def beet_import(album_dir: Path):
+def beet_import(album_dir: Path, custom_config: BytesIO):
     ''' Emulates the behaviour of calling Beets' import function from a shell, in an embedded
     fashion. This bypasses a lot of the overhead required in creating a new subprocess as well as
     for other set up. Since the default beets config and library are used, no custom processing is
@@ -45,13 +39,17 @@ def beet_import(album_dir: Path):
 
     Args:
         album_dir (Path): A path to the directory where the music was downloaded
+        config_file (BytesIO): A stream containing a custom beets configuration
     '''
+    # Patch the config to change the set_file function
+    setattr(beetsconfig, 'set_file', set_file_monkeypatch)
     # Create fake namespaces that would normally be created by parsing command line args
-    setup_options = SimpleNamespace(directory=None, config=None, plugins=None, library=None)
+    setup_options = SimpleNamespace(directory=None, config=custom_config, plugins=None, library=None)
     import_options = SimpleNamespace(copy=None, library=None)
-
+    # Start the import
     _, plugins, library = _setup(setup_options)
     import_func(library, import_options, [str(album_dir)])
+    # Clean up
     plugins.send('cli_exit', lib=library)
     library._close()
     beetsconfig.clear()
