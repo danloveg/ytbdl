fc7280e567919f5a3f8da6fdee83e503e777ffff
Daniel Lovegrove <d.lovegrove11@gmail.com>
2022-01-23 16:41:32 -0600

    Simplify config code, add documentation

--- src/ytbdl/__init__.py
+++ src/ytbdl/__init__.py
@@ -5,15 +5,28 @@ import confuse
 config = confuse.LazyConfig('ytbdl', None)
 
 def get_loaded_config_sources():
+    ''' Get existing configuration files
+
+    Returns:
+        (list): A list of (string) paths to configuration files that exist on
+            the file system. Returns an empty list if no configuration files
+            exist
+    '''
     config.resolve()
-    config_files = []
-    for source in config.sources:
-        if source.filename and os.path.exists(source.filename):
-            config_files.append(source.filename)
-    return config_files
+    return [s.filename for s in config.sources if os.path.exists(s.filename)]
 
 def get_main_config_path():
+    ''' Get the main configuration file path
+
+    Returns:
+        (str): A path to the configuration file. This path may or may not exist
+    '''
     return os.path.join(config.config_dir(), 'config.yaml')
 
 def config_exists():
-    return bool(get_loaded_config_sources())
+    ''' Determine if one or more configuration files exist.
+
+    Returns:
+        (bool): True if a config file exists, False otherwise
+    '''
+    return any(get_loaded_config_sources())
