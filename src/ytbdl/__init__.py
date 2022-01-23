d33ec9e0dd9e08aa427f9506f67882ea4b15d395
Daniel Lovegrove <d.lovegrove11@gmail.com>
2022-01-23 16:31:46 -0600

    Fix config_exists incorrectly reporting config existence

--- src/ytbdl/__init__.py
+++ src/ytbdl/__init__.py
@@ -1,5 +1,6 @@
+import os
+
 import confuse
-from pathlib import Path
 
 config = confuse.LazyConfig('ytbdl', None)
 
@@ -7,12 +8,12 @@ def get_loaded_config_sources():
     config.resolve()
     config_files = []
     for source in config.sources:
-        if source.filename and Path(source.filename).exists:
+        if source.filename and os.path.exists(source.filename):
             config_files.append(source.filename)
     return config_files
 
 def get_main_config_path():
-    return Path(config.config_dir()) / 'config.yaml'
+    return os.path.join(config.config_dir(), 'config.yaml')
 
 def config_exists():
     return bool(get_loaded_config_sources())
