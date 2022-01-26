2cafdd794554d45acc6ca97dce08bcbd26b60804
Daniel Lovegrove <d.lovegrove11@gmail.com>
2022-01-25 21:12:21 -0600

    Bump up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ytbdl',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     scripts=[],
